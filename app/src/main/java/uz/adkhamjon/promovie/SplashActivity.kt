fec62738ad1dbb28fd89a7c6971d0cd7cefeccb5
adkhamjonDev <adxam933>
2022-01-11 19:35:44 +0500

    Initial commit

---
+++ app/src/main/java/uz/adkhamjon/promovie/SplashActivity.kt
@@ -0,0 +1,32 @@
+package uz.adkhamjon.promovie
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+
+import uz.adkhamjon.promovie.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding:ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.status_bar_color_splash)
+        binding= ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val background: Thread = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(1500)
+                    val intent= Intent(applicationContext,MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } catch (e: Exception) {
+                }
+            }
+        }
+        background.start()
+
+    }
+}
